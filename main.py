9451cf8ed1abe55f654a466949fe7cfa0a678568
Juan Bindez <juanbindez780@gmail.com>
2023-05-16 15:41:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-'''
-Author: www.github.com/JuanBindez
-Description: qrcode generator
-Python Version: 3.10
-year: 2022
-Local: Brazil
-'''
-# sudo apt-get install python3-tk
+# MIT License
+#
+# Copyright (c) 2022 - 2023 Juan Bindez  <juanbindez780@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 from cProfile import label
 import pyqrcode
@@ -29,8 +43,6 @@ def converter():
     messagebox.showinfo("QRcode Converter", "Seu QRCODE Esta Pronto")
 
 
-
-#inicio do bloco da interface grafica
 window = Tk()
 
 window.title("Gerador De QRcode")
@@ -52,4 +64,3 @@ botao.grid(row=4, column=1)
 
 
 window.mainloop()
-#fim do bloco da interface grafica
